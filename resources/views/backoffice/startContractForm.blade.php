faad0f796df12abd6dd5832f4f95eb569bca04c5
vassilidev <vassilidevnet@gmail.com>
2024-02-27 22:44:55 +0100

    feat: add a progress bar

--- resources/views/backoffice/startContractForm.blade.php
+++ resources/views/backoffice/startContractForm.blade.php
@@ -4,6 +4,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped" id="progressBar" role="progressbar"></div>
+                </div>
                 <div id="form"></div>
             </div>
             <div class="col-md-6">
@@ -17,17 +20,42 @@
 
 @push('js')
     <script>
-        Formio.createForm(document.getElementById('form'), @js($contract->form->form_schema));
+        Formio.createForm(document.getElementById('form'), @js($contract->form->form_schema))
+            .then(function (form) {
+                var navigation = document.querySelector('.formio-wizard-navigation');
+                if (navigation) {
+                    navigation.style.display = 'none';
+                }
+
+                let totalPages = form.pages.length;
+
+                updateProgressBar(0, totalPages);
+
+                form.on('nextPage', (page) => updateProgressBar(page.page))
+                form.on('prevPage', (page) => updateProgressBar(page.page))
+
+                function updateProgressBar(currentPage) {
+                    let percentage = Math.floor((currentPage + 1) / totalPages * 100);
+
+                    document.getElementById('progressBar').style.width = percentage + '%';
+                    document.getElementById('progressBar').innerText = percentage + '%';
+                }
+            })
     </script>
 @endpush
 
 @push('css')
     <style>
+        .formio-form ul.pagination {
+            display: none;
+        }
+
         .iframe-container {
             position: relative;
             width: 100%;
             padding-top: 56.25%; /* Adjust this value according to your iframe's aspect ratio */
         }
+
         .iframe-container iframe {
             position: absolute;
             top: 0;
