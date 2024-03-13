15819b84b4f2ae7ea0a62510a31dd1f14973a230
vassilidev <vassilidevnet@gmail.com>
2024-03-13 21:12:05 +0100

    feat: add two charts

---
+++ app/Filament/Widgets/OrderChart.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OrderChart extends ChartWidget
+{
+    protected static ?string $heading = 'Orders';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Order::class)
+            ->between(
+                start: now()->subMonth(),
+                end: now(),
+            )
+            ->perDay()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Order per day',
+                    'data'  => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels'   => $data->map(fn(TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
---
+++ app/Filament/Widgets/OrderContractChart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Facades\DB;
+
+class OrderContractChart extends ChartWidget
+{
+    protected static ?string $heading = 'Order Contract';
+
+    protected function getData(): array
+    {
+        $orderCountsPerContractType = Order::select('contracts.name as contract_name', DB::raw('count(orders.id) as order_count'))
+            ->leftJoin('contracts', 'orders.contract_id', '=', 'contracts.id')
+            ->groupBy('contracts.name')
+            ->get();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Orders per Contract',
+                    'data'  => $orderCountsPerContractType->pluck('order_count')
+                ],
+            ],
+            'labels'   => $orderCountsPerContractType->pluck('contract_name')
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
