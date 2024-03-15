425101d7a8d2d92ea70eacfaa6079575f18f6c75
vassilidev <vassilidevnet@gmail.com>
2024-03-15 22:08:39 +0100

    feat: add more font size

--- resources/views/livewire/update-contract.blade.php
+++ resources/views/livewire/update-contract.blade.php
@@ -27,6 +27,7 @@
             plugins: 'directionality anchor autoresize autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
             toolbar: 'helpers ltr rtl lineheight | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align | numlist bullist indent outdent | emoticons charmap | removeformat',
             line_height_formats: '0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0',
+            font_size_formats: '1pt 2pt 3pt 4pt 5pt 6pt 7pt 8pt 9pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 17pt 18pt 19pt 20pt 21pt 22pt 23pt 24pt 25pt 26pt 27pt 28pt 29pt 30pt 31pt 32pt 33pt 34pt 35pt 36pt 37pt 38pt 39pt 40pt 41pt 42pt 43pt 44pt 45pt 46pt 47pt 48pt 49pt 50pt',
             setup: function (editor) {
                 var items = [];
 
