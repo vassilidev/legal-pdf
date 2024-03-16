3dfd093e7b5ab2146d28b69ba41d416ac7133f6e
vassilidev <vassilidevnet@gmail.com>
2024-03-16 01:43:01 +0100

    fix: add dir to render

--- resources/views/livewire/display-contract-form.blade.php
+++ resources/views/livewire/display-contract-form.blade.php
@@ -14,7 +14,7 @@
     </div>
 
     @if($data)
-        <div class="border border-top-0 border-4 shadow p-5" id="render">
+        <div class="border border-top-0 border-4 shadow p-5" dir="{{ $contract->direction }}" id="render">
             {!! $contract->render($data) !!}
         </div>
     @endif
