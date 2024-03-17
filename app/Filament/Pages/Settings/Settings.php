2e6b45fa3b7380ada010e07fbd02486f52459ce2
vassilidev <vassilidevnet@gmail.com>
2024-03-17 23:31:34 +0100

    fix: permission

--- app/Filament/Pages/Settings/Settings.php
+++ app/Filament/Pages/Settings/Settings.php
@@ -5,11 +5,17 @@ namespace App\Filament\Pages\Settings;
 use Closure;
 use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Textarea;
+use Illuminate\Support\Facades\Auth;
 use Outerweb\FilamentSettings\Filament\Pages\Settings as BaseSettings;
 
 class Settings extends BaseSettings
 {
     protected static ?string $navigationGroup = 'Settings';
+
+    public static function canAccess(): bool
+    {
+        return Auth::user()->can('page_Settings');
+    }
 
     public function schema(): array|Closure
     {
